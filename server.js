0905ec0d1eefd184e2c0a0f7d9496cb95664cc9a
Djak Pearce <djakpearce@MacBookPro.lan>
2025-02-07 09:43:59 -0500

    established routes, verified using postman, next is creating user model.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express ();
 
+// Connect Databse
+connectDB();
+
 app.get('/', (req, res)=>res.send(`API Running`));
+
+// Define Routes
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 const PORT = process.env.PORT || 8000;
 
