e2a2a46e679a11ec70d0ffb3b467702b6058215d
Djak Pearce <djakpearce@Djaks-iMac.lan>
2025-02-10 10:59:17 -0500

    Finished with function to create/update a user profile. Reminder-still not sure why the checks do not work as expected in post man.  Support articles suggest perhaps linked to connectivity issues.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -77,35 +77,56 @@ router.post(
             twitter,
             instagram,
             linkedin,
+        } = req.body;
 
-      } = req.body;
+        // Build profile object
+        const profileFields = {};
+        profileFields.user = req.user.id;
 
-      // Build profile object
-      const profileFields = {};
-      profileFields.user = req.user.id;
+        if(company) profileFields.company = company;
+        if(website) profileFields.website = website;
+        if(location) profileFields.location = location;
+        if(bio) profileFields.bio = bio;
+        if(status) profileFields.status = status;
+        if(githubusername) profileFields.githubusername = githubusername;
+        
+        // Convert comma-separated list to Array
+        if(skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+        
+        // Test to make sure skills is returned properly and that the route is working
+        // console.log(profileFields.skills);
 
-      if(company) profileFields.company = company;
-      if(website) profileFields.website = website;
-      if(location) profileFields.location = location;
-      if(bio) profileFields.bio = bio;
-      if(status) profileFields.status = status;
-      if(githubusername) profileFields.githubusername = githubusername;
-      
-      // Convert comma-separated list to Array
-      if(skills) {
-        profileFields.skills = skills.split(',').map(skill => skill.trim());
-      }
-    
-      // Test to make sure skills is returned properly and that the route is working
-      console.log(skills);
-      res.send('Hello');
+        // Build Social object
+        profileFields.social = {}
+        if(youtube) profileFields.social.youtube = youtube;
+        if(facebook) profileFields.social.facebook = facebook;
+        if(twitter) profileFields.social.twitter = twitter;
+        if(instagram) profileFields.social.instagram = instagram;
+        if(linkedin) profileFields.social.linkedin = linkedin;
 
-      if(youtube) profileFields.youtube = youtube;
-      if(facebook) profileFields.facebook = facebook;
-      if(twitter) profileFields.twitter = twitter;
-      if(instagram) profileFields.instagram = instagram;
-      if(linkedin) profileFields.linkedin = linkedin;
+        try {
+            let profile = await Profile.findOne({ user: req.user.id });
+            if(profile) {
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields }, 
+                    { new: true }
+                );
 
+                return res.json(profile);
+            }
+
+            // Create
+            profile = new Profile(profileFields);
+            await profile.save();
+            res.json(profile);
+
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
 });
 
 module.exports = router;
